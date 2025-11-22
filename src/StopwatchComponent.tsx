83d2c00e818d6a098f3f9c6b462b6c819c36e08f
Daniel Alexander Nedergaard <danieldownfarm@gmail.com>
2025-11-22 15:52:14 +0100

    Start, stop and reset seems to work as intended now, but resume isn't working yet

--- src/StopwatchComponent.tsx
+++ src/StopwatchComponent.tsx
@@ -1,38 +1,53 @@
 import { useRef, useState, useEffect } from 'react'
 
 export default function StopwatchComponent() {
-    const [elapsedTime, setElapsedTime] = useState(0);
+    const [elapsedTime, setElapsedTime] = useState<number>(0);
+    const [isRunning, setIsRunning] = useState<boolean>(false);
     const startTimeRef = useRef<number>(0);
-    const timerRef = useRef<number | null>(null);
+    const timerRef = useRef<number>(0);
     
     function initiateStopwatch() {
+        setIsRunning(true);
         startTimeRef.current = new Date().getTime();
+
+        timerRef.current = window.setInterval(() => {
+            setElapsedTime((new Date().getTime() - startTimeRef.current) / 1000) // the / 1000 converts milliseconds to seconds
+        }, 1000);
     }
 
     function stopStopwatch() {
-        if (timerRef.current !== null) {
-            clearInterval(timerRef.current);
-            timerRef.current = null;
-        }
+        setIsRunning(false);
+        clearInterval(timerRef.current);
+    }
+
+    function resumeStopwatch() {
+        setIsRunning(true);
+        
+        timerRef.current = window.setInterval(() => {
+            setElapsedTime((new Date().getTime() - startTimeRef.current) / 1000) // the / 1000 converts milliseconds to seconds
+        }, 1000);
     }
 
     function resetStopwatch() {
         setElapsedTime(0);
+        timerRef.current = 0;
     }
-
-    useEffect(() => {
-        timerRef.current = window.setInterval(() => {
-            
-        }, 1000);
-        setElapsedTime((new Date().getTime() - startTimeRef.current) / 1000) // the / 1000 converts milliseconds to seconds
-    });
 
     return (
         <div className="card">
             <span id="time">{elapsedTime.toFixed(0)}s</span>
-            <button id="start-button" onClick={initiateStopwatch}>Start</button>
-            <button id="stop-button" onClick={stopStopwatch}>Stop</button>
-            <button id="reset-button" onClick={resetStopwatch}>nulstil</button>
+            {isRunning === false && timerRef.current === 0 && (
+                <button id="start-button" onClick={initiateStopwatch}>Start</button> // show start button when timer is not running
+            )}
+            {isRunning && (
+                <button id="stop-button" onClick={stopStopwatch}>Stop</button>
+            )}
+            {isRunning === false && timerRef.current > 0 && (
+                <button id="resume-button" onClick={resumeStopwatch}>Geonoptag</button>
+            )}
+            {isRunning === false && timerRef.current > 0 && (
+                <button id="reset-button" onClick={resetStopwatch}>Nulstil</button>
+            )}
         </div>
     );
 }
