8d5b0468fd962b3e5fb37a1e9f0dcb7f6e93a2b9
Daniel Alexander Nedergaard <danieldownfarm@gmail.com>
2025-11-30 07:27:52 +0100

    Rewrote stopwatchcomponent

--- src/StopwatchComponent.tsx
+++ src/StopwatchComponent.tsx
@@ -1,38 +1,38 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export default function StopwatchComponent() {
     const [elapsedTime, setElapsedTime] = useState<number>(0);
     const [isRunning, setIsRunning] = useState<boolean>(false);
     const startTimeRef = useRef<number>(0);
-    const timerRef = useRef<number>(0);
-    
+
+    useEffect (() => {
+        let interval = undefined;
+
+        if (isRunning) {
+            interval = setInterval(() => {
+                setElapsedTime(new Date().getTime() - startTimeRef.current);
+            }, 100);
+        }
+        return () => clearInterval(interval);
+    }, [isRunning]);
+
     function initiateStopwatch() {
         setIsRunning(true);
         startTimeRef.current = new Date().getTime();
-
-        timerRef.current = window.setInterval(() => {
-            setElapsedTime(new Date().getTime() - startTimeRef.current)
-        }, 100);
     }
 
     function stopStopwatch() {
         setIsRunning(false);
-        clearInterval(timerRef.current);
     }
 
     function resumeStopwatch() {
         setIsRunning(true);
 
         startTimeRef.current = new Date().getTime() - elapsedTime; // moving the startTime to match time passed
-
-        timerRef.current = window.setInterval(() => {
-            setElapsedTime(new Date().getTime() - startTimeRef.current)
-        }, 1000);
     }
 
     function resetStopwatch() {
         setElapsedTime(0);
-        timerRef.current = 0;
     }
 
     function formatMsToHHMMSS(ms: number): string {
@@ -41,9 +41,9 @@ export default function StopwatchComponent() {
         let seconds = Math.floor((ms / 1000) % 60);
 
         // adds leading zero
-        let hoursStr = ("00" + hours).slice(-2);
-        let minutesStr = ("00" + minutes).slice(-2);
-        let secondsStr = ("00" + seconds).slice(-2);
+        let hoursStr = ("0" + hours).slice(-2);
+        let minutesStr = ("0" + minutes).slice(-2);
+        let secondsStr = ("0" + seconds).slice(-2);
 
         return hoursStr + ":" + minutesStr + ":" + secondsStr;
     }
@@ -51,16 +51,16 @@ export default function StopwatchComponent() {
     return (
         <div className="card">
             <span id="time">{formatMsToHHMMSS(elapsedTime)}</span>
-            {isRunning === false && timerRef.current === 0 && (
+            {isRunning === false && elapsedTime === 0 && (
                 <button id="start-button" onClick={initiateStopwatch}>Start</button> // show start button when timer is not running and time is 0
             )}
             {isRunning && (
                 <button id="stop-button" onClick={stopStopwatch}>Stop</button> // show stop button when timer is running
             )}
-            {isRunning === false && timerRef.current > 0 && (
+            {isRunning === false && elapsedTime > 0 && (
                 <button id="resume-button" onClick={resumeStopwatch}>Geonoptag</button> // show resume button when timer is not running and time is greater than 0
             )}
-            {isRunning === false && timerRef.current > 0 && (
+            {isRunning === false && elapsedTime > 0 && (
                 <button id="reset-button" onClick={resetStopwatch}>Nulstil</button> // show reset button when timer is not running and time is greater than 0
             )}
         </div>
